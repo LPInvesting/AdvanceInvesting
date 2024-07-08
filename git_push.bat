bec38a11e0091571d381c910ebe375a91794f35c
Luiz Pedro <lpedro.work@gmail.com>
2024-07-08 10:08:22 -0300

    Descri├º├úo das novas altera├º├Áes

---
+++ git_push.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Adicionar todos os arquivos modificados
+git add .
+
+:: Fazer o commit com uma mensagem padrão ou personalizada
+git commit -m "Descrição das novas alterações"
+
+:: Enviar as alterações para o repositório remoto
+git push origin main
+pause
