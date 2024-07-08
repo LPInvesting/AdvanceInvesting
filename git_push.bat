98f38e1d98e63cabe71497df5e145c090ec7a9c1
Luiz Pedro <lpedro.work@gmail.com>
2024-07-08 14:07:35 -0300

    %commit_msg%

--- git_push.bat
+++ git_push.bat
@@ -3,9 +3,9 @@
 git add .
 
 :: Fazer o commit com uma mensagem padrão ou personalizada
-git commit -m "Descrição das novas alterações"
+set /p commit_msg="Digite a mensagem do commit (ou pressione Enter para usar a mensagem padrão): "
+if "%commit_msg%"=="" set commit_msg=Descrição das novas alterações
+git commit -m "%commit_msg%"
 
 :: Enviar as alterações para o repositório remoto
 git push -u origin main
-
-pause
